3d8b28440b3833ff5e14f9b53898b30ee62af638
yujuc <yujuc.tw@gmail.com>
2020-12-14 23:46:10 +0900

    Add react-resort project

---
+++ react-resort/src/components/FeaturedRooms.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {RoomContext} from '../context';
+import Title from './Title';
+import Loading from './Loading';
+import Room from './Room';
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+    render() {
+        let {loading, featuredRooms: rooms} = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+        
+        return (
+            <section className="featured-room">
+                <Title title="featured rooms" />
+                <div className="featured-room-center">
+                    {loading?  <Loading /> : rooms}
+                </div>
+            </section>
+        )
+    }
+}
